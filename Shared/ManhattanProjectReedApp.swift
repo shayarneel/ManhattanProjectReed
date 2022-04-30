32c703918f5f0cf96c0331c2ba1a9d94fb7c5535
Shayarneel Kundu <shayarneel@gmail.com>
2022-04-29 21:33:10 -0500

    Initial Commit

---
+++ Shared/ManhattanProjectReedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManhattanProjectReedApp.swift
+//  Shared
+//
+//  Created by Shayarneel Kundu on 4/29/22.
+//
+
+import SwiftUI
+
+@main
+struct ManhattanProjectReedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
